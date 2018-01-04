2bcb2a064c688ecc2b06a7f1b5777b59266433fb
kuckboy1994 <shanchao@qq.com>
2018-01-04 13:21:42 +0800

    父子组件

---
+++ src/components/father.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>我是父组件</h1>
+    <button @click="receiveStart">开始请求数据吧</button>
+    <son :start="start"></son>
+  </div>
+</template>
+
+<script>
+import son from '@/components/son'
+export default {
+  components: {son},
+  data () {
+    return {
+      start: false
+    }
+  },
+  methods: {
+    receiveStart () {
+      this.start = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/son.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>我是子组件</h2>
+    {{start ? '父组件让我请求数据': '父组件没让我请求数据'}}
+    <br>
+    {{receiveData == '' ? '我还没收到数据呢': receiveData}}
+  </div>
+</template>
+
+<script>
+export default {
+	props: {
+		start: {
+	      type: Boolean,
+	      default: false
+	    },
+	},
+	data () {
+		return {
+			receiveData: ''
+		}
+	},
+	methods: {
+		requestData () {
+			setTimeout(()=> {
+				this.receiveData = '我是收到的数据'
+			}, 1000)
+		}
+	},
+	watch: {
+		start: function (newval) {
+			if (newval) {
+				this.requestData()
+			}
+		}
+	},
+	// mounted () {
+	// 	this.requestData()
+	// }
+}
+</script>
+
+<style scoped>
+
+</style>
