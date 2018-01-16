9da765d8f907b49e7f0c41074c1992448bd69910
kuckboy1994 <shanchao@qq.com>
2018-01-16 18:48:25 +0800

    11

--- src/components/father.vue
+++ src/components/father.vue
@@ -5,12 +5,20 @@
     <son :start="start"></son>
 
     <div class="computed">
-    <h1>演示computed</h1>
+      <h1>演示computed</h1>
 
-    <input type="text" placeholder="数字1" v-model="num1">
-    <input type="text" placeholder="数字2" v-model="num2">
-    <input type="text" placeholder="数字3" v-model="num3">
-    <input type="text" placeholder="得分" v-model="num4">
+      <input type="text" placeholder="数字1" v-model="num1">
+      <input type="text" placeholder="数字2" v-model="num2">
+      <input type="text" placeholder="数字3" v-model="num3">
+      <input type="text" placeholder="得分" v-model="num4">
+    </div>
+    <br>
+    <div class="watch">
+      <h1>演示watch</h1>
+
+      <input type="text" placeholder="数字1" v-model="obj.a">
+      <input type="text" placeholder="数字2" v-model="obj.b">
+      <input type="text" placeholder="数字3" v-model="obj.c">
     </div>
   </div>
 </template>
@@ -25,6 +33,11 @@ export default {
       num1: '',
       num2: '',
       num3: '',
+      obj: {
+        a: 100,
+        b: '',
+        c: ''
+      }
     }
   },
   methods: {
@@ -36,6 +49,23 @@ export default {
     num4: function () {
       return Number(this.num1) + Number(this.num2) + Number(this.num3);
     }
+  },
+  watch: {
+    // obj: {
+    //   handler: function (val, oldVal) {
+    //     console.log(val, oldVal)
+    //   },
+    //   deep: true
+    // },
+    // 'obj.a': function (val, oldVal) {
+    //   console.log('new: %s, old: %s', val, oldVal)
+    // },
+    'obj.b': function (val, oldVal) {
+      this.obj.c = this.obj.a - Number(this.obj.b)
+    },
+    'obj.c': function (val, oldVal) {
+      this.obj.b = this.obj.a - Number(this.obj.c)
+    },
   }
 }
 </script>
@@ -43,6 +73,10 @@ export default {
 <style scoped>
 .computed {
   border: 1px solid red;
-  height: 300px;
+  height: 150px;
+}
+.watch {
+  border: 1px solid red;
+  height: 150px;
 }
 </style>
