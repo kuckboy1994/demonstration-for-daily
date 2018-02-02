27d7b1c7c3b8d86627eeccda62ce87071c3428c8
kuckboy1994 <shanchao@qq.com>
2018-02-02 19:39:39 +0800

    anchorlink

---
+++ src/components/anchorlink.vue
@@ -0,0 +1,66 @@
+<template>
+    <div>
+        <ul class="left">
+            <li v-for="(item, index) in leftlist" 
+                :class="{'selected': index === selectIndex}" 
+                @click="changeSelect(index)"
+                :key="index">{{item}}</li>
+        </ul>
+        <div class="content">
+            <section v-for="(item, index) in leftlist" 
+                class="section-article"
+                :key="index">page {{item}}</section>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            leftlist: [1,2,3,4,5,6,7,8,9,10],
+            selectIndex: 0,
+        }
+    },
+    created () {
+        document.title = '平滑跳转锚链接'
+    },
+    methods: {
+        changeSelect (index) {
+            this.selectIndex = index
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+.left {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    width: 20vw;
+    background: palegoldenrod;
+    overflow: auto;
+}
+.left li {
+    height: 100px;
+    line-height: 100px;
+    border-bottom: 1px solid blueviolet;
+}
+.selected {
+    background: blue;
+    color: white;
+}
+.section-article {
+    background: saddlebrown;
+    width: 80vw;
+    margin-left: 20vw;
+    height: 300px;
+    color: white;
+    border-bottom: 1px solid black;
+    margin-bottom: 20px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import fatherCom from '@/components/father'
 import sectransferPage from '@/components/sectransferPage'
+import anchorlinkPage from '@/components/anchorlink'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/sectransfer',
       name: 'sectransfer',
       component: sectransferPage
+    },
+    {
+      path: '/anchorlink',
+      name: 'anchorlink',
+      component: anchorlinkPage
     },
   ]
 })
